4fc1c30a47931c5bb59e74ebb98730fc08aef6ed
StepperOK <parnuk1996@gmail.com>
2021-07-01 17:34:10 +0300

    Ui&Bll

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 import { NavLink } from 'react-router-dom';
+import Message from './Message/Message';
+import DialogItem from './DialogItem/DialogItem';
 
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-const Message = (props) => {
-    return <div className={s.dialog}>{props.message} </div>
-}
 
 const Dialogs = (props) => {
     let dialogs = [
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,7 +10,7 @@ const MyPosts = (props) => {
     { id: 3, message: 'Hi, how are you?', likesCount: 14 },
     { id: 4, message: 'It`s my first post', likesCount: 11 }
   ]
-  let postsElements = posts.map(p => <Post message={p.message} LikesCount={p.likesCount} />);
+  let postsElements = posts.map(p => <Post message={p.message} LikesCount={p.likesCount} />); //Берет посты из массива
 
   return <div className={s.postsBlock}>
     <h3> My posts</h3>
